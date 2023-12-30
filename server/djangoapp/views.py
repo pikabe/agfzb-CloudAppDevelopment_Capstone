1d1809f50b9fe3db49d5bdab63bb30a115806d90
pikabe <35191200+pikabe@users.noreply.github.com>
2023-12-30 17:07:24 +0000

    about_us

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,9 +18,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
-
+def about(request):
+    return render(request, 'djangoapp/about.html')
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
